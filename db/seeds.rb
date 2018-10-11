3ca0bb75ad18263e8167ab6ea6100f71205d758d
Tyler Fisher <tfisher21@gmail.com>
2018-10-11 18:58:42 -0500

    Migrate database for Product model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+#   Product: name:string, price:integer, image_url:string, description:string, population:integer, country:string, language:string
+
+Product.create([
+  {
+    name: "Tokyo",
+    price: 997,
+    image_url: ,
+    description: ,
+    popultion: ,
+    country: ,
+    language: ,
+  },
+  {
+    name: ,
+    price: ,
+    image_url: ,
+    description: ,
+    popultion: ,
+    country: ,
+    language: ,
+  },
+  {
+    name: ,
+    price: ,
+    image_url: ,
+    description: ,
+    popultion: ,
+    country: ,
+    language: ,
+  },
+  {
+    name: ,
+    price: ,
+    image_url: ,
+    description: ,
+    popultion: ,
+    country: ,
+    language: ,
+  },
+  {
+    name: ,
+    price: ,
+    image_url: ,
+    description: ,
+    popultion: ,
+    country: ,
+    language: ,
+  },
+])
