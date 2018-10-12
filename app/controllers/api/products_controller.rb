f9277410bca4b2b6455ccdcca3ac9189fcc6b5dc
Tyler Fisher <tfisher21@gmail.com>
2018-10-11 19:33:48 -0500

    Create route, controller, and jbuilder to display first product, last product, and random product.

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,20 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jbuilder"
   end
+
+  def first_product
+    @product = Product.first
+    render "first_product.json.jbuilder"
+  end
+
+  def last_product
+    @product = Product.first
+    render "last_product.json.jbuilder"
+  end
+
+  def random_product
+    random_id = rand(5) + 1
+    @product = Product.find_by(id: random_id)
+    render "random_product.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-    get "/all_products" => "products#all_products"
+    get "/all_products"   => "products#all_products"
+    get "/first_product"  => "products#first_product"
+    get "/last_product"   => "products#last_product"
+    get "/random_product"   => "products#random_product"
   end
 end
