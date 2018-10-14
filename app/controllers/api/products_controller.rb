06438fb6653c43efe491f839dd3b9ece812e1371
Tyler Fisher <tfisher21@gmail.com>
2018-10-14 12:01:43 -0500

    Refactor product routes to allow search via params

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,19 +4,9 @@ class Api::ProductsController < ApplicationController
     render "all_products.json.jbuilder"
   end
 
-  def first_product
-    @product = Product.first
-    render "first_product.json.jbuilder"
-  end
+  def product
+    @product = Product.find_by("name": params[:name])
 
-  def last_product
-    @product = Product.first
-    render "last_product.json.jbuilder"
-  end
-
-  def random_product
-    random_id = rand(5) + 1
-    @product = Product.find_by(id: random_id)
-    render "random_product.json.jbuilder"
+    render "product.json.jbuilder"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     get "/all_products"   => "products#all_products"
-    get "/first_product"  => "products#first_product"
-    get "/last_product"   => "products#last_product"
-    get "/random_product"   => "products#random_product"
+    get "/product/:name"  => "products#product"
+    get "/product"        => "products#product"
   end
 end
