bc3a85f21cc3d0430bb73d36a5b6cbb71d06a546
Tyler Fisher <tfisher21@gmail.com>
2018-10-16 19:01:04 -0500

    Add destroy action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -41,4 +41,11 @@ class Api::ProductsController < ApplicationController
 
     render "show.json.jbuilder"
   end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+
+    render json: {message: "And BOOM! goes the city!"}
+  end
 end
