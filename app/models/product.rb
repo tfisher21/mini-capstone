b6d0b4dd6a23573d729b4e6d2a28588cd1cb86f2
Tyler Fisher <tfisher21@gmail.com>
2018-10-21 09:52:10 -0500

    Create basic validation checks for Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  validates :city_name, presence: true
+  validates :city_name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0}
+  validates :description, length: {in: 10..500}
+
+
   def is_discounted?
     price < 900
   end
