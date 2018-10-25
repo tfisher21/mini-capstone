99d1662d95a78166d491d6a0ce032bbeb34e62d7
Tyler Fisher <tfisher21@gmail.com>
2018-10-25 18:46:37 -0500

    refactor authentication

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,38 +1,31 @@
 class Api::OrdersController < ApplicationController
+  before_action :authenticate_user
   def index
-    if current_user
-      @orders = current_user.orders
-      render "index.json.jbuilder"
-    else
-      render json: {}, status: :unauthorized
-    end
+    @orders = current_user.orders
+    render "index.json.jbuilder"
   end
 
   def create
-    if current_user
-      product = Product.find_by(id: params[:product_id])
-      price = product.price
-      subtotal = price * params[:quantity].to_i
-      tax = subtotal * 0.08
-      total = subtotal + tax
+    product = Product.find_by(id: params[:product_id])
+    price = product.price
+    subtotal = price * params[:quantity].to_i
+    tax = subtotal * 0.08
+    total = subtotal + tax
 
 
-      @order = Order.new(
-        product_id: params[:product_id],
-        quantity: params[:quantity],
-        user_id: current_user.id,
-        subtotal: subtotal,
-        tax: tax,
-        total: total
-      )
+    @order = Order.new(
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      user_id: current_user.id,
+      subtotal: subtotal,
+      tax: tax,
+      total: total
+    )
 
-      if @order.save
-        render "show.json.jbuilder"
-      else
-        render json: {errors: @order.errors.full_messages}
-      end
+    if @order.save
+      render "show.json.jbuilder"
     else
-      render json: {}, status: :unauthorized
+      render json: {errors: @order.errors.full_messages}
     end
   end
 end
