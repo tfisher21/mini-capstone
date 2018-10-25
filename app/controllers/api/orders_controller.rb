fad17cbb443bbda8b418bddf845f091226e9b59b
Tyler Fisher <tfisher21@gmail.com>
2018-10-25 18:39:13 -0500

    Add authentication in order create and index methods

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,32 +1,38 @@
 class Api::OrdersController < ApplicationController
   def index
-    @orders = current_user.orders
-
-    render "index.json.jbuilder"
+    if current_user
+      @orders = current_user.orders
+      render "index.json.jbuilder"
+    else
+      render json: {}, status: :unauthorized
+    end
   end
 
   def create
-    
-    product = Product.find_by(id: params[:product_id])
-    price = product.price
-    subtotal = price * params[:quantity].to_i
-    tax = subtotal * 0.08
-    total = subtotal + tax
+    if current_user
+      product = Product.find_by(id: params[:product_id])
+      price = product.price
+      subtotal = price * params[:quantity].to_i
+      tax = subtotal * 0.08
+      total = subtotal + tax
 
 
-    @order = Order.new(
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      user_id: current_user.id,
-      subtotal: subtotal,
-      tax: tax,
-      total: total
-    )
+      @order = Order.new(
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        user_id: current_user.id,
+        subtotal: subtotal,
+        tax: tax,
+        total: total
+      )
 
-    if @order.save
-      render "show.json.jbuilder"
+      if @order.save
+        render "show.json.jbuilder"
+      else
+        render json: {errors: @order.errors.full_messages}
+      end
     else
-      render json: {errors: @order.errors.full_messages}
+      render json: {}, status: :unauthorized
     end
   end
 end
