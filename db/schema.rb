a583969cc55695ad6d481e0cb06d3c2186f6086d
Tyler Fisher <tfisher21@gmail.com>
2018-10-25 19:25:01 -0500

    Create Admin for users and create authenticate admin method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_004601) do
+ActiveRecord::Schema.define(version: 2018_10_25_235446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2018_10_25_004601) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
